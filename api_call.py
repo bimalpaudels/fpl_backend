bf67c6c62b23ba1fa3d7db728aa57714d2ed2b3a
Bimal Paudel <ibimalp@gmail.com>
2024-08-09 13:59:43 +0200

    Refactor: Updated the scripts with validation and serialization from Pydantic

--- api_call.py
+++ api_call.py
@@ -1,7 +1,9 @@
 import enum
+import time
+
 import requests
 from db_config import db_connection
-from utils import filter_required_attributes, filtered_players_details, player_history_mock_data
+from utils import validated_required_attributes, filtered_players_details, player_history_mock_data
 from typing import Tuple, Optional
 from psycopg.rows import dict_row
 
@@ -10,12 +12,36 @@ TRACKED_FIELDS = ['now_cost', 'second_name']
 
 
 class ActionType(enum.Enum):
+    """
+    Enumeration of possible actions for player data from remote and local database.
+    """
     CREATE = 'CREATE'
     UPDATE = 'UPDATE'
     NO_ACTION = 'NO_ACTION'
 
 
-def get_player_detail_api(player_id: int):
+def call_bootstrap_api():
+    """
+    The main api of Fantasy Premier League that returns everything.
+    :return: Currently only returns the players data but others will be utilized in the future.
+    """
+    url = BASE_URL + 'bootstrap-static/'
+    response = requests.get(url)
+    if response.status_code == 200:
+        response = response.json()
+        players = response['elements']
+        return players
+    else:
+        print("Something went wrong")
+        return
+
+
+def call_player_detail_api(player_id: int):
+    """
+    Get individual player details from remote api and return the history object.
+    :param player_id: Player id from local database
+    :return history: The history of individual player details for the season.
+    """
     url = BASE_URL + f'element-summary/{player_id}/'
     response = requests.get(url)
     if response.status_code == 200:
@@ -25,29 +51,27 @@ def get_player_detail_api(player_id: int):
     elif response.status_code == 429:
         print('Too many request')
         breakpoint()
+        return
 
 
-def get_bootstrap_api():
+def get_players():
     """
-    The main api of Fantasy Premier League that returns everything. The response is separated into players and
-    their respective teams.
-    :return: Response object of the bootstrap-static api
+    Function to get all players data calling the call_bootstrap_api function and insert into local database.
+    :return: Currently only returns success message on console.
     """
-    response = requests.get(BASE_URL+'bootstrap-static/').json()
-
-    # Separate players
-    players = response['elements']
-    filtered_players_data = filter_required_attributes(players)
+    players = call_bootstrap_api()
+    filtered_players_data = validated_required_attributes(players)
     upsert_players(filtered_players_data)
     # Clean players to only take data required.
-    return filtered_players_data
+    return True
 
 
 def upsert_players(players):
-    # Use players ID to check if the player exists
-    # If player matching the id doesn't exist, insert operation
-    # If player matching the id exists, compare the data
-    # If there are changes, UPDATE else leave it as it is.
+    """
+    Function called from get_players() to insert or update into local database
+    :param players: List of players from get_players()
+    :return: Print statement to console showing the count of the data manipulated.
+    """
     inserted_count = 0
     updated_count = 0
     unchanged_count = 0
@@ -75,7 +99,7 @@ def upsert_players(players):
         else:
             unchanged_count += 1
 
-    print(f"Inserted: {inserted_count}, updated: {updated_count}, unchanged: {unchanged_count}")
+    print(f"New Players: {inserted_count}, Updated: {updated_count}, Unchanged: {unchanged_count}")
 
 
 def verify_player_exists(player_detail) -> Tuple[ActionType, Optional[dict]]:
@@ -87,21 +111,15 @@ def verify_player_exists(player_detail) -> Tuple[ActionType, Optional[dict]]:
     query = """SELECT * FROM players WHERE player_id =%s"""
     with connection as conn:
         with conn.cursor(row_factory=dict_row) as cursor:
-
             record = cursor.execute(query, (player_id,)).fetchone()
 
     if record is None:
         return ActionType.CREATE, None
-
     changed_values = {}
 
-    for field in TRACKED_FIELDS:
-        remote_value = player_detail.get(field)
-        local_value = record.get(field)
-
-        if remote_value != local_value:
-            changed_values[field] = remote_value
-
+    for key, value in player_detail.items():
+        if value != record.get(key):
+            changed_values[key] = value
     if changed_values:
         return ActionType.UPDATE, changed_values
 
@@ -119,24 +137,31 @@ def get_player_stats_by_gw():
     with db_connection() as conn:
         results = conn.execute(query).fetchone()
     player_ids = results[0]
-    for player_id in range(1, 20):
-        # data = get_player_detail_api(player_id)
-        # data and  player_ids will be used in the future
-        history = player_history_mock_data(player_id)
-        filtered_player_data = filtered_players_details(history)
-        upsert_player_stats_by_gw(filtered_player_data)
-    # Mock will be replaced by actual API call later.
+    for i in range(0, len(player_ids),  50):
+        chunk = player_ids[i:i+50]
+        for player_id in chunk:
+            # data = call_player_detail_api(player_id)
+
+            # data and  player_ids will be used in the future
+            history = player_history_mock_data(player_id)
+            filtered_player_data = filtered_players_details(history)
+            upsert_player_stats_by_gw(filtered_player_data)
+        print("Waiting 100 seconds...")
+        time.sleep(1)
+
+    return True
 
 
 def upsert_player_stats_by_gw(player_data):
     inserted_count = 0
     updated_count = 0
     unchanged_count = 0
+    player = ""
 
     for data in player_data:
-        # Check if that gw data exists for that player (1, 182)
+        player = data['player_id']
         action_type, changed_data = verify_player_gw_exists(data)
-        # If it doesn't exist execute CREATE
+
         if action_type == ActionType.CREATE:
             columns = ', '.join(data.keys())
             placeholders = ', '.join(['%s'] * len(data))
@@ -145,12 +170,20 @@ def upsert_player_stats_by_gw(player_data):
             with db_connection() as conn:
                 conn.execute(query, values)
             inserted_count += 1
-        # If it does exist check for changes
-        # If there is changes, get the changes and update
-        # Else just skip
+
+        elif action_type == ActionType.UPDATE:
+            set_clause = ", ".join(f"{key} = %s" for key in changed_data.keys())
+            values = list(changed_data.values()) + [data['player_id']] + [data['game_week']]
+            query = f"UPDATE players_detail SET {set_clause} WHERE player_id =%s AND game_week =%s"
+            with db_connection() as conn:
+                conn.execute(query, values)
+            updated_count += 1
+
         else:
             unchanged_count += 1
-    print(f"Inserted: {inserted_count}, updated: {updated_count}, unchanged: {unchanged_count}")
+
+    if inserted_count != 0 or updated_count != 0:
+        print(f"{player} -> New gw: {inserted_count}, updated: {updated_count}, unchanged: {unchanged_count}")
 
 
 def verify_player_gw_exists(player_data) -> Tuple[ActionType, Optional[dict]]:
@@ -159,13 +192,34 @@ def verify_player_gw_exists(player_data) -> Tuple[ActionType, Optional[dict]]:
 
     query = """SELECT * FROM players_detail WHERE player_id =%s AND game_week =%s"""
     with db_connection() as conn:
-        data = conn.execute(query, (player_id, game_week)).fetchone()
+        with conn.cursor(row_factory=dict_row) as cursor:
+            data = cursor.execute(query, (player_id, game_week)).fetchone()
+
     if data is None:
         return ActionType.CREATE, None
-    else:
-        return ActionType.NO_ACTION, None
+
+    changed_values = {}
+
+    for key, value in player_data.items():
+
+        if value != data.get(key):
+            changed_values[key] = value
+
+    if changed_values:
+        return ActionType.UPDATE, changed_values
+
+    return ActionType.NO_ACTION, None
 
 
 if __name__ == "__main__":
-    # get_bootstrap_api()
-    get_player_stats_by_gw()
+    try:
+        if get_players():
+            print("Successfully loaded players from remote.")
+        if get_player_stats_by_gw():
+            print("Successfully loaded individual gw data.")
+        print("Scripts executed!!!")
+
+    except Exception as e:
+        print(e)
+
+
--- players/schemas.py
+++ players/schemas.py
@@ -3,18 +3,19 @@ from typing import Optional
 
 
 class PlayerSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     first_name: str
     second_name: str
     now_cost: int
     player_id: int
 
-    class Config:
-        orm_mode = True
-
 
 class PlayersDetailSchema(BaseModel):
-    player_id: int
-    game_week: int
+    model_config = ConfigDict(from_attributes=True)
+
+    player_id: int = Field(..., alias='element')
+    game_week: int = Field(..., alias='round')
     assists: int
     total_points: int
     minutes: int
@@ -39,15 +40,11 @@ class PlayersDetailSchema(BaseModel):
     transfers_in: int
     transfers_out: int
 
-    class Config:
-        orm_mode = True
-
 
 class PlayerDetailResponse(PlayerSchema):
-    details: list[PlayersDetailSchema]
+    model_config = ConfigDict(from_attributes=True)
 
-    class Config:
-        orm_mode = True
+    details: list[PlayersDetailSchema]
 
 
 class PlayerSelectedPercentageSchema(PlayerSchema):
@@ -86,3 +83,44 @@ class PlayerListResponseSchema(PlayerSchema):
     red_cards: int
     saves: int
     bonus: int
+
+
+class PlayerCompleteSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    player_id: int = Field(..., alias='id')
+    first_name: str
+    second_name: str
+    now_cost: int
+    form: float
+    selected_by_percent: float
+    total_points: int
+    minutes: int
+    goals_scored: int
+    assists: int
+    clean_sheets: int
+    goals_conceded: int
+    own_goals: int
+    penalties_saved: int
+    penalties_missed: int
+    yellow_cards: int
+    red_cards: int
+    saves: int
+    bonus: int
+    bps: int
+    influence: float
+    creativity: float
+    threat: float
+    ict_index: float
+    starts: int
+    expected_goals: float
+    expected_assists: float
+    expected_goals_conceded: float
+    expected_goals_involvement: float = Field(..., alias='expected_goal_involvements')
+    expected_goals_per_90: float
+    saves_per_90: float
+    expected_assists_per_90: float
+    expected_goals_involvements_per_90: float = Field(..., alias='expected_goal_involvements_per_90')
+    expected_goals_conceded_per_90: float
+    goals_conceded_per_90: float
+    points_per_game: float
