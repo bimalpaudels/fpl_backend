4753bc5539d75c26b0f6b36b183bf0c735c1c9dc
Bimal Paudel <ibimalp@gmail.com>
2024-08-28 08:27:18 +0200

    Feat: Organized Kubernetes config files

--- players/database.py
+++ players/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql+psycopg://postgres:postgres@localhost:5432/fpl_db')
+SQLALCHEMY_DATABASE_URL = os.getenv('ALCHEMY_DB_URL', 'postgresql+psycopg://postgres:postgres@localhost:5432/fpl_db')
 # SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg://postgres:postgres@localhost:5432/fpl_db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
